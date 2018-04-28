ac14bb57ee50f98ce7a3e8ecca586b2a83786bf6
marcosradix <marcosradix@gmail.com>
2018-04-27 23:22:52 -0300

    projeto angular 5 com primeng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-
+import {CalendarModule} from 'primeng/calendar';
+import {InputTextModule} from 'primeng/inputtext';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    CalendarModule,
+    InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
